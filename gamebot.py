6e7c67a852da5b6ce0e52d7ead4eb161c88c98dc
noonbles <atrac480@gmail.com>
2022-09-08 16:43:59 -0400

    test run gitignore

--- gamebot.py
+++ gamebot.py
@@ -1,2 +1,12 @@
 import discord
+import os
 
+
+client = discord.client()
+
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+
+client.run('MTAxNzUxOTExMTk5MzA1MzE4NA.G8bZuE.f_nK55X2gEwKZWBsDYz8051c1ePzGcPLwxjdwc')
